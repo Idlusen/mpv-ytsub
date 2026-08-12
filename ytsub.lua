65f3a16828f7b6eb29a4ceea1a44e53cffa173a5
Idlusen <boulanger.m@mailoo.org>
2026-08-12 22:30:04 +0200

    defaulting the source_lang option to "en" so that mpv does not ignore it

--- ytsub.lua
+++ ytsub.lua
@@ -14,7 +14,7 @@ local http = want("socket.http")
 local https = want("ssl.https")
 
 local options = {
-    source_lang = nil, -- en fr es de and the like
+    source_lang = 'en', -- en fr es de and the like
     load_autosub_binding = "alt+y",
     autoload_autosub_binding = "alt+Y",
     cache_dir = ".cache/ytsub/",
